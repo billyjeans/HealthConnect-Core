27d3933bd30098de348ee92cbd9d4b780f553b51
biah01 <bilal.ahmed@amova.eu>
2024-10-03 20:54:04 +0200

    first commit

---
+++ src/main/java/com/healthconnect/core/HealthConnectCoreApplication.java
@@ -0,0 +1,13 @@
+package com.healthconnect.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthConnectCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthConnectCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/healthconnect/core/HealthConnectCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthconnect.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthConnectCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
